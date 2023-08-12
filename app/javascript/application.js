e7da25ddc9b74d1e60d1277d1f76bdbb93e1f7dd
Jephte Extra <jephteextra@gmail.com>
2023-08-11 22:03:35 -0400

    install and configure bootstrap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
-import { Turbo } from "@hotwired/turbo-rails";
+// import { Turbo } from "@hotwired/turbo-rails";
 // Turbo.session.drive = false;
 import "controllers";
 import "popper";
 import "bootstrap";
+
+var popoverTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="popover"]')
+);
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl);
+});
