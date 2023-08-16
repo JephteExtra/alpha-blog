3a4bf07e0ea8f7448a5a1aa45d318334fd5107e8
Jephte Extra <jephteextra@gmail.com>
2023-08-16 12:18:33 -0400

    Add restrictions to user and articles index and show views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,7 +21,7 @@ class ArticlesController < ApplicationController
   def create
     # render plain: params[:article]
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:notice] = "Article was created successfully" # Where an error occurs use flash[:alert]
       # redirect_to article_path(@article)
